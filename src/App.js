f14d62c1165cfa9c5bf593b30da6ec82e101b33a
Priyanshu Mishra <priyanshu12651265@gmail.com>
2021-05-30 02:45:54 +0530

    1. Did another test change coz I am dumb

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         shia is the besttttttttttttttttttttttttttttttttttttttttt and shushi
+         shia is the besttttttttttttttttttttttttttttttttttttttttt and shushi puchiiiiiiiiiii
         </p>
         <a
           className="App-link"
