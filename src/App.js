baf0461b04791d2b2346220ecb91004ee91faf17
Priyanshu Mishra <priyanshu12651265@gmail.com>
2021-06-06 19:26:20 +0530

    1.Added MainContent, 2.Added Empty files for Feature Protoype

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 import Navbar from "./resources/navbar/navbar"
 import Footer from "./resources/footer/footer"
+import MainContent from "./resources/maincontent/maincontent"
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <MainContent/>
       <Footer/>
     </div>
   );
