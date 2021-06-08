fadcc0decdbd5beb8e5866f56107630cf7ed9f3c
Arshia Sandhu <arshiasandhu07@gmail.com>
2021-06-08 22:44:59 +0530

    1. Refined feature templates. 2. Removed test link from navbar.

---
+++ src/resources/maincontent/feature/featureprotoflip.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./feature.css"
+
+function FeatureProtoFlip (props){
+    return (
+       <div className="featwrapper vflex flip">
+           <div className="featimg flipimg" style={{backgroundImage:`url("${props.fimage}")`}}>
+
+           </div>
+           <div className="featinfo flipinfo">
+               <h3>{props.heading}</h3>
+               <p>{props.description}</p>
+           </div>
+       </div>
+    )
+}
+export default FeatureProtoFlip
