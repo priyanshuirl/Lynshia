60d7ee7590df1993e64ea356b11f867077a25ddf
Priyanshu Mishra <priyanshu12651265@gmail.com>
2021-06-18 05:06:58 +0530

    1.Added Facebook Open Graph Cards (beta)

--- src/resources/maincontent/feature/featureprotoflip.js
+++ src/resources/maincontent/feature/featureprotoflip.js
@@ -4,9 +4,7 @@ import "./feature.css"
 function FeatureProtoFlip (props){
     return (
        <div className="featwrapper vflex flip">
-           <div className="featimg flipimg" style={{backgroundImage:`url("${props.fimage}")`}}>
-
-           </div>
+           <div className="featimg flipimg" style={{backgroundImage:`url("${props.fimage}")`}}></div>
            <div className="featinfo flipinfo">
                <h3>{props.heading}</h3>
                <p>{props.description}</p>
