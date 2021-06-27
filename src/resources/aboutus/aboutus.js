5f857f4b3d976acc2a2d66219ef7133640e17121
Priyanshu Mishra <priyanshu12651265@gmail.com>
2021-06-27 23:02:02 +0530

    1.Compressed Image, 2.Decreased Animated Div speed, 3.Fixed Purple div size, 4.Fixed purple div link

--- src/resources/aboutus/aboutus.js
+++ src/resources/aboutus/aboutus.js
@@ -1,10 +1,12 @@
-import React, {useEffect} from "react"
+import React, { useEffect } from "react"
 import './aboutus.css';
+
+import { Link } from "react-router-dom"
 
 function AboutUs() {
     useEffect(() => {
         window.scrollTo(0, 0)
-      }, [])
+    }, [])
     return (
         <div>
             <div class="aboutbanner vflex">
@@ -13,16 +15,18 @@ function AboutUs() {
             <div class="contentbanner vflex">
                 <p class="head">Lynshia is one of the World's Most Innovative and Fastest Growing women's health platforms.</p>
                 <p class="content">
-                    Menstruation has always been considered a taboo in mainstream society and often times we find women struggling with this even in the 21st Century many times even risking their health due to lack of awareness and misinformation.<br/><br/>
+                    Menstruation has always been considered a taboo in mainstream society and often times we find women struggling with this even in the 21st Century many times even risking their health due to lack of awareness and misinformation.<br /><br />
                     We here at Lynshia break the stereotypes and Provide crucial assistance and support to women across the Globe during those times through our virtual assistant.
-                   </p>
-                   <p class="tagbanner tgban"> Our goal is to empower every single woman during her menstruation and Beyond.</p>
-                   <p class="content"> We built a Smart Personal Assistant that provides a plethora of features and crucial assistance to women during their Menstruation, from Tacking and predicting their cycles to helping manage and reduce their cramps while providing help and support through our Dedicated FAQ's, tips, tricks section, and Reminders.</p>
+                </p>
+                <p class="tagbanner tgban"> Our goal is to empower every single woman during her menstruation and Beyond.</p>
+                <p class="content"> We built a Smart Personal Assistant that provides a plethora of features and crucial assistance to women during their Menstruation, from Tacking and predicting their cycles to helping manage and reduce their cramps while providing help and support through our Dedicated FAQ's, tips, tricks section, and Reminders.</p>
             </div>
-            <div className="purplediv vflex">
-        <h2>Making Periods Easier to Manage.</h2>
-        <a href="{\#}"><h4>Sign Up Today!</h4></a>
-    </div>
+            <Link to="/comingsoon">
+                <div className="purplediv vflex">
+                    <h2>Making Periods Easier to Manage.</h2>
+                    <h4>Sign Up Today!</h4>
+                </div>
+            </Link>
         </div>
     )
 }
