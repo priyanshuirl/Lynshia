1dd9d0d07c66b1d0c69f97389518cf2e83c1f78e
Priyanshu Mishra <priyanshu12651265@gmail.com>
2021-07-14 01:49:33 +0530

    1.Added Youtube Embedd Video

--- src/resources/maincontent/maincontent.js
+++ src/resources/maincontent/maincontent.js
@@ -35,6 +35,9 @@ function MainContent() {
                     <h1>But with Lynshia by your side it's never been easier!</h1>
                 </div>
             </div>
+            <div className="iframecontainer">
+            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/_LI3syHaByM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
             <div className="tile">
                 <h5>What Can Lynshia Do For You?</h5>
             </div>
